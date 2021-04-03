28a274e6536674b2cd3aa4f3ca881045bb610cb4
Jing <trashyiiiiiii@gmail.com>
2021-04-03 18:18:38 -0400

    First Commit

---
+++ desktop/src/com/sakkiwakki/beattype/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.sakkiwakki.beattype.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.sakkiwakki.beattype.BeatType;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new BeatType(), config);
+	}
+}
